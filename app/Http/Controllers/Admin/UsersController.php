475edd83bdffae2141c86cf656a00a38c7b667ab
SU1_Basiir <softwareu1@i-smart.com.sg>
2022-11-21 11:53:41 +0800

    initial

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\MediaUploadingTrait;
+use App\Http\Requests\MassDestroyUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\Role;
+use App\Models\User;
+use Gate;
+use Illuminate\Http\Request;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\HttpFoundation\Response;
+
+class UsersController extends Controller
+{
+    use MediaUploadingTrait;
+
+    public function index()
+    {
+        abort_if(Gate::denies('user_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $users = User::with(['roles', 'media'])->get();
+
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        abort_if(Gate::denies('user_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $roles = Role::pluck('title', 'id');
+
+        return view('admin.users.create', compact('roles'));
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        $user = User::create($request->all());
+        $user->roles()->sync($request->input('roles', []));
+        if ($request->input('photo_upload', false)) {
+            $user->addMedia(storage_path('tmp/uploads/' . basename($request->input('photo_upload'))))->toMediaCollection('photo_upload');
+        }
+
+        if ($request->input('file_upload', false)) {
+            $user->addMedia(storage_path('tmp/uploads/' . basename($request->input('file_upload'))))->toMediaCollection('file_upload');
+        }
+
+        if ($media = $request->input('ck-media', false)) {
+            Media::whereIn('id', $media)->update(['model_id' => $user->id]);
+        }
+
+        return redirect()->route('admin.users.index');
+    }
+
+    public function edit(User $user)
+    {
+        abort_if(Gate::denies('user_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $roles = Role::pluck('title', 'id');
+
+        $user->load('roles');
+
+        return view('admin.users.edit', compact('roles', 'user'));
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        $user->roles()->sync($request->input('roles', []));
+        if ($request->input('photo_upload', false)) {
+            if (!$user->photo_upload || $request->input('photo_upload') !== $user->photo_upload->file_name) {
+                if ($user->photo_upload) {
+                    $user->photo_upload->delete();
+                }
+                $user->addMedia(storage_path('tmp/uploads/' . basename($request->input('photo_upload'))))->toMediaCollection('photo_upload');
+            }
+        } elseif ($user->photo_upload) {
+            $user->photo_upload->delete();
+        }
+
+        if ($request->input('file_upload', false)) {
+            if (!$user->file_upload || $request->input('file_upload') !== $user->file_upload->file_name) {
+                if ($user->file_upload) {
+                    $user->file_upload->delete();
+                }
+                $user->addMedia(storage_path('tmp/uploads/' . basename($request->input('file_upload'))))->toMediaCollection('file_upload');
+            }
+        } elseif ($user->file_upload) {
+            $user->file_upload->delete();
+        }
+
+        return redirect()->route('admin.users.index');
+    }
+
+    public function show(User $user)
+    {
+        abort_if(Gate::denies('user_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $user->load('roles');
+
+        return view('admin.users.show', compact('user'));
+    }
+
+    public function destroy(User $user)
+    {
+        abort_if(Gate::denies('user_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $user->delete();
+
+        return back();
+    }
+
+    public function massDestroy(MassDestroyUserRequest $request)
+    {
+        User::whereIn('id', request('ids'))->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function storeCKEditorImages(Request $request)
+    {
+        abort_if(Gate::denies('user_create') && Gate::denies('user_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $model         = new User();
+        $model->id     = $request->input('crud_id', 0);
+        $model->exists = true;
+        $media         = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
+
+        return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
+    }
+}
